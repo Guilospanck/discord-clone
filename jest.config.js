30b3e1daa47470ec36c8e8aef178f181da9fb330
Guilherme Rodrigues <guilherme.rp2010@hotmail.com>
2022-03-27 17:33:58 -0300

    refactor: jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,9 @@ module.exports = {
   roots: ['<rootDir>/src'],
   globals: {},
   collectCoverageFrom: [
+    '<rootDir>/src/**/*.tsx',
     '<rootDir>/src/**/*.ts',
+    '<rootDir>/src/**/styles.ts',
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
