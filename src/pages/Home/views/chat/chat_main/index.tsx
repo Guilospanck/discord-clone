f38b3476dfadcd0c1dcb4d8a46de65854bd8f01b
Guilherme Rodrigues <guilherme.rp2010@hotmail.com>
2022-03-30 18:59:16 -0300

    feat: chat main content

--- src/pages/Home/views/chat/chat_main/index.tsx
+++ src/pages/Home/views/chat/chat_main/index.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 import { UseChatMainViewModelReturnType } from '~/pages/Home/viewModels/chat/useChatMainViewModel'
 
 import {
-  Article
+  Article,
+  Container,
+  Avatar,
+  MessageContainer,
+  UsernameAndTimestampContainer,
+  Username,
+  TimestampContainer,
+  Timestamp,
+  MessageContent
 } from './styles'
 
 type ChatMainViewProps = {
@@ -11,11 +19,64 @@ type ChatMainViewProps = {
 
 export const ChatMainView = ({ }: ChatMainViewProps) => {
 
+  const mockedText = 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'
+
   return (
     <Article>
-      Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-      Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-      Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+      <MessageContainer>
+        <Avatar
+          src="https://cdn.discordapp.com/avatars/759237066999201852/e780bd32be2d3b5e78179ddc8da20e77.webp?size=80"
+          aria-hidden="true"
+          alt=" "
+        />
+        <UsernameAndTimestampContainer>
+          <Username>guilospanck</Username>
+          <TimestampContainer>
+            <Timestamp>Today at 6:11pm</Timestamp>
+          </TimestampContainer>
+        </UsernameAndTimestampContainer>
+        <MessageContent>
+          {mockedText}
+        </MessageContent>
+      </MessageContainer>
+
+      <MessageContainer>
+        <Avatar
+          src="https://cdn.discordapp.com/avatars/759237066999201852/e780bd32be2d3b5e78179ddc8da20e77.webp?size=80"
+          aria-hidden="true"
+          alt=" "
+        />
+        <UsernameAndTimestampContainer>
+          <Username>guilospanck</Username>
+          <TimestampContainer>
+            <Timestamp>Today at 6:11pm</Timestamp>
+          </TimestampContainer>
+        </UsernameAndTimestampContainer>
+        <MessageContent>
+          {mockedText}
+        </MessageContent>
+      </MessageContainer>
+
+      <MessageContainer>
+        <Avatar
+          src="https://cdn.discordapp.com/avatars/759237066999201852/e780bd32be2d3b5e78179ddc8da20e77.webp?size=80"
+          aria-hidden="true"
+          alt=" "
+        />
+        <UsernameAndTimestampContainer>
+          <Username>guilospanck</Username>
+          <TimestampContainer>
+            <Timestamp>Today at 6:11pm</Timestamp>
+          </TimestampContainer>
+        </UsernameAndTimestampContainer>
+        <MessageContent>
+          {mockedText}
+        </MessageContent>
+      </MessageContainer>
+
+
+
     </Article>
   )
 
--- src/pages/Home/views/chat/chat_main/styles.ts
+++ src/pages/Home/views/chat/chat_main/styles.ts
@@ -1,16 +1,116 @@
 import styled from "styled-components";
 
+const NORMAL_TEXT_COLOR = '#dcddde'
+const NORMAL_TEXT_MUTED = '#72767d'
+const HEADER_PRIMARY_COLOR = '#fff'
+
 export const Article = styled.article`
   display: flex;
   flex-direction: column;
   flex: 1 1 auto;
 
-  padding: 16px;
+  padding: 16px 0;
+  margin-bottom: 6px;
 
   align-items: stretch;
   justify-content: stretch;
   overflow-x: none;
   overflow-y: auto;
-  overflow-anchor: none; // when new data appears, it won't move the scrollbar
+  overflow-anchor: none; // when new data appears, it won't move the scrollbar    
+`
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  overflow: hidden;
+  position: relative;
+  margin-top: 1.0625rem;
+`
+
+export const Avatar = styled.img`
+  position: absolute;
+  left: 16px;
+  margin-top: calc(10px - .125rem);
+  z-index: 1;
+
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  overflow: hidden;
+  cursor: pointer;
+
+  user-select: none;
+  flex: 0 0 auto;
+  pointer-events: none;
+`
+
+export const MessageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 0 0 auto;
+
+  min-height: 2.75rem;
+  padding-left: 72px;
+  padding-top: .125rem;
+  padding-bottom: .125rem;
+  padding-right: 48px !important;
+
+  position: relative;
+`
+
+export const UsernameAndTimestampContainer = styled.h2`
+  display: block;
+  flex-direction: row;
+
+  margin: 0;
+
+  overflow: hidden;
+  line-height: 1.375rem;
+  min-height: 1.375rem;
+  white-space: break-spaces;
+`
+
+export const Username = styled.span`
+  margin-right: .25rem;
   
+  font-size: 1rem;
+  font-weight: 500;
+  line-height: 1.375rem;
+  color: ${HEADER_PRIMARY_COLOR};
+  display: inline;
+  vertical-align: baseline;
+  overflow: hidden;
+  flex-shrink: 0;
+`
+
+export const TimestampContainer = styled.span`
+  font-size: 0.75rem;
+  line-height: 1.375rem;
+  color: ${NORMAL_TEXT_MUTED};
+  vertical-align: baseline;
+
+  margin-left: .25rem;
+
+  display: inline-block;
+  height: 1.25rem;
+  cursor: default;
+  pointer-events: none;
+  font-weight: 500;
+`
+
+export const Timestamp = styled.time`
+
+`
+
+export const MessageContent = styled.div`
+  user-select: text; 
+  color: ${NORMAL_TEXT_COLOR};
+  font-size: 1rem;
+  line-height: 1.375rem;
+  white-space: pre-wrap;
+  white-space: break-spaces;
+  word-wrap: break-word;
+  user-select: text;
+  font-weight: 400;
 `
