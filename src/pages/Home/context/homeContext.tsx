805f7170757b24096b1bba65807cdb73af06f2f7
Guilherme Rodrigues <guilherme.rp2010@hotmail.com>
2022-04-01 10:08:30 -0300

    refactor: adding iterator

--- src/pages/Home/context/homeContext.tsx
+++ src/pages/Home/context/homeContext.tsx
@@ -8,7 +8,12 @@ type HomeContextProps = {
   allServers: ServerInfo[],
   serverSelected: ServerInfo,
   setServerSelected: React.Dispatch<React.SetStateAction<ServerInfo>>,
-  BACKGROUND_POSITIONS: BGSizeCoordinates[]
+  backgroundPositionsIterator: () => IBackgroundPositionsIterator
+}
+
+interface IBackgroundPositionsIterator {
+  hasValue: (idx: number) => boolean
+  next: () => BGSizeCoordinates
 }
 
 export const HomeContext = createContext<HomeContextProps | null>(null)
@@ -17,42 +22,68 @@ export const HomeContextProvider: FunctionComponent = ({ children }) => {
   const [allServers] = useState<ServerInfo[]>(serversMocked)
   const [serverSelected, setServerSelected] = useState<ServerInfo>(serversMocked[0])
 
-  const BACKGROUND_POSITIONS: BGSizeCoordinates[] = useMemo(() => ([
-    {
-      x: 0,
-      y: 0
-    },
-    {
-      x: -88,
-      y: -88
-    },
-    {
-      x: -88,
-      y: 0
-    },
-    {
-      x: -88,
-      y: -22
-    },
-    {
-      x: -88,
-      y: -44
-    },
-    {
-      x: -198,
-      y: -22
-    },
-    {
-      x: 0,
-      y: -22
+  const backgroundPositionsIterator = useMemo(() => {
+    const coordinates = [
+      {
+        x: 0,
+        y: 0
+      },
+      {
+        x: -88,
+        y: -88
+      },
+      {
+        x: -88,
+        y: 0
+      },
+      {
+        x: -88,
+        y: -22
+      },
+      {
+        x: -88,
+        y: -44
+      },
+      {
+        x: -198,
+        y: -22
+      },
+      {
+        x: 0,
+        y: -22
+      }
+    ]
+
+    let index = 0
+
+    const iterator = (): IBackgroundPositionsIterator => {
+      const hasValue = (idx: number): boolean => {
+        return coordinates[idx] !== undefined
+      }
+
+      const next = (): BGSizeCoordinates => {
+        if (hasValue(index + 1)) {
+          index++
+        } else {
+          index = 0
+        }
+        return coordinates[index]
+      }
+
+      return {
+        hasValue,
+        next
+      }
     }
-  ]), [])
+
+    return iterator
+  }, [])
 
   const defaultContext: HomeContextProps = {
     allServers,
     serverSelected,
     setServerSelected,
-    BACKGROUND_POSITIONS
+    backgroundPositionsIterator
   }
 
   return <HomeContext.Provider value={defaultContext}>{children}</HomeContext.Provider>
--- src/pages/Home/viewModels/chat/useChatFormViewModel.ts
+++ src/pages/Home/viewModels/chat/useChatFormViewModel.ts
@@ -14,11 +14,10 @@ export type UseChatFormViewModelReturnType = {
 }
 
 export const useChatFormViewModel = (): UseChatFormViewModelReturnType => {
-  const { BACKGROUND_POSITIONS } = useContext(HomeContext)
+  const { backgroundPositionsIterator } = useContext(HomeContext)
 
   const messageRef = useRef<HTMLDivElement>(null)
   const [emojiCoordinates, setEmojiCoordinates] = useState<EmojiCoordinatesType>({ x: 0, y: 0 })
-  const emojiCoordinatesIndexRef = useRef(0)
 
   const onKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
     if (e.key === 'Enter' && !e.shiftKey) {
@@ -28,18 +27,8 @@ export const useChatFormViewModel = (): UseChatFormViewModelReturnType => {
   }
 
   const onMouseEnterEmojiBtn = useCallback(() => {
-    console.log('hey')
-    // const index = Math.floor(Math.random() * BACKGROUND_POSITIONS.length)
-
-    /** Iterator */
-    const currentIndex = emojiCoordinatesIndexRef.current
-    let newIndex = currentIndex + 1
-    if (newIndex >= BACKGROUND_POSITIONS.length) {
-      newIndex = 0
-    }
-
-    emojiCoordinatesIndexRef.current = newIndex
-    setEmojiCoordinates(BACKGROUND_POSITIONS[newIndex])
+    const nextCoordinates = backgroundPositionsIterator().next()
+    setEmojiCoordinates(nextCoordinates)
   }, [])
 
   return {
