15d7a0c2a930a439e73ac3bb14bf81e7882735b3
Guilherme Rodrigues <guilherme.rp2010@hotmail.com>
2022-04-03 19:20:25 -0300

    refactor: updating for gh-pages

--- src/Route.tsx
+++ src/Route.tsx
@@ -4,8 +4,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 
 export const AppRoute = () => {
+
+  const BASENAME = process.env.NODE_ENV === 'development' ? '/' : '/discord-clone'; // because of github pages
+
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={BASENAME}>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
